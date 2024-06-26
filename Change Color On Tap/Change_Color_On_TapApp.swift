18674e104abe1438a26731d5368f9c64df321a2c
iSAD <bradx2713@gmail.com>
2024-06-26 15:08:28 +0200

    Initial Commit

---
+++ Change Color On Tap/Change_Color_On_TapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Change_Color_On_TapApp.swift
+//  Change Color On Tap
+//
+//  Created by Tafadzwa Alexander on 2024/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Change_Color_On_TapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
