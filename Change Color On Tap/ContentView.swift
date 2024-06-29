207f609d9e0424ab6b521c72a347e1d72a5a2263
iSAD <bradx2713@gmail.com>
2024-06-29 07:13:05 +0200

    Some improvements!

--- Change Color On Tap/ContentView.swift
+++ Change Color On Tap/ContentView.swift
@@ -15,90 +15,95 @@ struct ContentView: View {
     @State var circleColor4:Color = .gray
     @State var selectedCircle = ""
     @State var answerText = "Check Answer"
+    @State var markScore = 0
     
     var body: some View {
         VStack{
             Text("Who coded this?")
                 .font(.system(size: 30))
-            HStack {
-                Circle()
-                    .fill(
-                    Color(circleColor)
-                    )
-                    .frame(width: 20, height: 20)
-                    .onTapGesture {
-                        circleColor = .blue
-                        circleColor2 = .gray
-                        circleColor3 = .gray
-                        circleColor4 = .gray
-                        selectedCircle = "A"
-                        
+            VStack(alignment: .leading){
+                HStack {
+                    Circle()
+                        .fill(
+                            Color(circleColor)
+                        )
+                        .frame(width: 20, height: 20)
+                        .onTapGesture {
+                            circleColor = .blue
+                            circleColor2 = .gray
+                            circleColor3 = .gray
+                            circleColor4 = .gray
+                            selectedCircle = "A"
+                            
+                        }
+                    Text("A.    Alexander")
+                        .font(.system(size: 20))
+                }.padding(.bottom)
+                HStack {
+                    Circle()
+                        .fill(
+                            Color(circleColor2)
+                        )
+                        .frame(width: 20, height: 20)
+                        .onTapGesture {
+                            circleColor = .gray
+                            circleColor2 = .blue
+                            circleColor3 = .gray
+                            circleColor4 = .gray
+                            selectedCircle = "B"
+                        }
+                    Text("B.    Bobby")
+                        .font(.system(size: 20))
+                }.padding(.bottom)
+                
+                HStack {
+                    Circle()
+                        .fill(
+                            Color(circleColor3)
+                        )
+                        .frame(width: 20, height: 20)
+                        .onTapGesture {
+                            circleColor = .gray
+                            circleColor2 = .gray
+                            circleColor3 = .blue
+                            circleColor4 = .gray
+                            selectedCircle = "C"
+                        }
+                    Text("C.    Tafadzwa")
+                        .font(.system(size: 20))
                 }
-                Text("A.    Alexander")
-                    .font(.system(size: 20))
-            }.padding(.bottom)
-            HStack {
-                Circle()
-                    .fill(
-                    Color(circleColor2)
-                    )
-                    .frame(width: 20, height: 20)
-                    .onTapGesture {
-                        circleColor = .gray
-                        circleColor2 = .blue
-                        circleColor3 = .gray
-                        circleColor4 = .gray
-                        selectedCircle = "B"
+                .padding(.bottom)
+                
+                HStack {
+                    Circle()
+                        .fill(
+                            Color(circleColor4)
+                        )
+                        .frame(width: 20, height: 20)
+                        .onTapGesture {
+                            circleColor = .gray
+                            circleColor2 = .gray
+                            circleColor3 = .gray
+                            circleColor4 = .blue
+                            selectedCircle = "D"
+                        }
+                    Text("D.    iSAD")
+                        .font(.system(size: 20))
                 }
-                Text("B.    Tafadzwa")
-                    .font(.system(size: 20))
-            }.padding(.bottom)
-            
-            HStack {
-                Circle()
-                    .fill(
-                    Color(circleColor3)
-                    )
-                    .frame(width: 20, height: 20)
-                    .onTapGesture {
-                        circleColor = .gray
-                        circleColor2 = .gray
-                        circleColor3 = .blue
-                        circleColor4 = .gray
-                        selectedCircle = "C"
-                }
-                Text("C.    Bobby")
-                    .font(.system(size: 20))
+                .padding(.bottom)
             }
-            .padding(.horizontal, -75)
-            .padding(.bottom)
-            
-            HStack {
-                Circle()
-                    .fill(
-                    Color(circleColor4)
-                    )
-                    .frame(width: 20, height: 20)
-                    .onTapGesture {
-                        circleColor = .gray
-                        circleColor2 = .gray
-                        circleColor3 = .gray
-                        circleColor4 = .blue
-                        selectedCircle = "D"
-                }
-                Text("D.    iSAD")
-                    .font(.system(size: 20))
-            }
-            .padding(.horizontal, -75)
-            .padding(.bottom)
             
             Button(action: {
                 answerCheck()
             }, label: {
                 Text(answerText)
             })
-
+            Text(String(markScore))
+                .font(.system(size: 40))
+                .padding(.top)
         }
+        .multilineTextAlignment(.trailing)
+        .padding()
     }
     
     func answerCheck(){
@@ -106,6 +111,16 @@ struct ContentView: View {
         if selectedCircle == "D" {
             circleColor4 = .green
             answerText = "Correct🥳!"
+            if markScore == 1 {
+                markScore += 0
+            }
+            else if markScore < 1 || markScore  == 0{
+                markScore += 1
+            }
+            else if markScore < 1{
+                markScore += 0
+            }
+            
         }
         
         else if selectedCircle == "A" && selectedCircle != ""{
@@ -122,8 +137,8 @@ struct ContentView: View {
             circleColor3 = .red
             answerText = "Wrong Answer😔!"
         }
-
-
+        
+        
     }
 }
 
